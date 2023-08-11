a161bd64db1f68333ec51d117903a8e5b468bd59
ferpa <feririarte.1996@gmail.com>
2023-08-11 20:34:37 -0300

    styles msg donation

--- Commands/Donar.js
+++ Commands/Donar.js
@@ -1,9 +1,10 @@
-// const Discord = require(`discord.js`);
+const Discord = require(`discord.js`);
 const Command = require(`./Command.js`);
 const UserManager = require(`../lnbitsAPI/UserManager.js`);
 const UserWallet = require(`../lnbitsAPI/User.js`);
 const { formatter } = require("../utils/helperFormatter.js");
 const { updateUserRank } = require("../database/DonateRank.js");
+const { AuthorConfig } = require("../utils/helperConfig.js");
 
 /*
 This command will show the balance of the mentioned user
@@ -67,14 +68,34 @@ class Donar extends Command {
               const payment = await uw.payInvoice(outgoingInvoice.invoice);
 
               if (payment) {
-                await updateUserRank(Interaction.user.id, "pozo", amount.value);
+                const updatedRank = await updateUserRank(
+                  Interaction.user.id,
+                  "pozo",
+                  amount.value
+                );
 
-                Interaction.editReply({
-                  content: `${senderData.toString()} ha donado ${formatter(
-                    0,
-                    2
-                  ).format(amount.value)} satoshis al pozo!`,
-                });
+                const embed = new Discord.MessageEmbed()
+                  .setColor(`#0099ff`)
+                  .setAuthor(AuthorConfig)
+                  .setURL(`https://wallet.lacrypta.ar`)
+                  .addFields(
+                    {
+                      name: `Donación a ${process.env.POOL_ADDRESS}`,
+                      value: `${senderData.toString()} ha donado ${formatter(
+                        0,
+                        2
+                      ).format(amount.value)} satoshis al pozo!`,
+                    },
+                    {
+                      name: "Total donado",
+                      value:
+                        updatedRank && updatedRank.amount
+                          ? `${updatedRank.amount}`
+                          : "0",
+                    }
+                  );
+
+                Interaction.editReply({ embeds: [embed] });
                 return;
               }
             }
@@ -82,7 +103,6 @@ class Donar extends Command {
             console.log(err);
             Interaction.editReply({
               content: `Ocurrió un error`,
-              ephemeral: true,
             });
             return;
           }
@@ -92,7 +112,6 @@ class Donar extends Command {
       } else {
         Interaction.editReply({
           content: `No tienes una billetera`,
-          ephemeral: true,
         });
       }
     } catch (err) {
